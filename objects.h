58c44758fb4691e0d7faced025bc9a560dbc2078
Jaime Thomas <avi.thomas@gmail.com>
2009-04-02 20:46:08 -0400

    Stupid tabs.

--- objects.h
+++ objects.h
@@ -20,32 +20,32 @@ class Universe
         
     protected:
 
-        void gravity();		//computes and applies gravity forces for all objects.
-        void air();		// takes care of air pressure on objects and air resistance.      
+        void gravity();         //computes and applies gravity forces for all objects.
+        void air();             // takes care of air pressure on objects and air resistance.      
 
-        void balence_forces();	//creats a list of all forces acting on an object and balences them.
+        void balence_forces();  //creats a list of all forces acting on an object and balences them.
         void update_positions(); 
 
         //etc...
 
     public:
 
-        double Gx, Gy;		//gravity force on all objects... earth style
-        double X, Y;		// origin of the universe ;
-        double temp;		// ambient tempature 
-        double p;		// atmospheric air pressure
+        double Gx, Gy;          //gravity force on all objects... earth style
+        double X, Y;            // origin of the universe ;
+        double temp;            // ambient tempature 
+        double p;               // atmospheric air pressure
         
-        double dT;		// rate of time passage in unviverse rellative to time in R.L. [Se/Si]
+        double dT;              // rate of time passage in unviverse rellative to time in R.L. [Se/Si]
 
 
-        void Step(const int t)	// move the universe t number of timesteps forward in time
-        void clear_objects ();	// delete all objects from the universe
+        void Step(const int t)  // move the universe t number of timesteps forward in time
+        void clear_objects ();  // delete all objects from the universe
         void delete_object (object *obj) ;
         
-	/*
-	 * all objects are stored in the universe. the universe is responsable for 
-	 * moderating all forces and interactions that occur between objects.
-	 */
+        /*
+         * all objects are stored in the universe. the universe is responsable for 
+         * moderating all forces and interactions that occur between objects.
+         */
 
 };
 
@@ -60,52 +60,52 @@ class object
    
         //---| Section containing physical properties of the objects material |----
 
-	const double Q;		// specific heat capacity of the material
-				// some constant that determines the strength of the material
-				// some constant that determines the elasticity of the material
-	double temp;		// temperature
-        double mass;		// or density maybe... maybe both
+        const double Q;         // specific heat capacity of the material
+                                // some constant that determines the strength of the material
+                                // some constant that determines the elasticity of the material
+        double temp;            // temperature
+        double mass;            // or density maybe... maybe both
         
-        double Ks, Kk		// coefficents of friction.
+        double Ks, Kk           // coefficents of friction.
         
         public:
 
         // ---| Section describing spacial properties of the object | ----
 
-        double x1, y1, z1;	// top left [1] and bottum right[2] vertices units meters
-	double x2, y2, z2;	// Etc..
-        double Vx, Vy, Vz;	// x, y, z components of the objects velocity. m/s
+        double x1, y1, z1;      // top left [1] and bottum right[2] vertices units meters
+        double x2, y2, z2;      // Etc..
+        double Vx, Vy, Vz;      // x, y, z components of the objects velocity. m/s
 
         /*
-	 * etc...
-	 * I dont care the this commenting style... its annoying.
-	 * You are annoying.  Suck it up and grow a pair.
-	 */
+         * etc...
+         * I dont care the this commenting style... its annoying.
+         * You are annoying.  Suck it up and grow a pair.
+         */
 
-	/* Hurrah for only rectangles at this point! */
-        void object(const double x,	// X coord
-		    const double y,	// Y coord
-		    const double z,	// Z coord
-		    const double l,	// length
-		    const double w,	// width
-		    const double d,	// depth
-                    string materials	// Predefined materials
-				    ); 
+        /* Hurrah for only rectangles at this point! */
+        void object(const double x,     // X coord
+                    const double y,     // Y coord
+                    const double z,     // Z coord
+                    const double l,     // length
+                    const double w,     // width
+                    const double d,     // depth
+                    string materials    // Predefined materials
+                                    ); 
 
-        void object(const double x,	// X coord
-		    const double y,	// Y coord
-		    const double z,	// Z coord
-		    const double l,	// length
-		    const double w,	// width
-		    const double d,	// depth
-                    const double q,	// specific heat capacity
-		    const double S,	// no idea
-		    const double E,	// ...?
-		    const double density
-				    );
+        void object(const double x,     // X coord
+                    const double y,     // Y coord
+                    const double z,     // Z coord
+                    const double l,     // length
+                    const double w,     // width
+                    const double d,     // depth
+                    const double q,     // specific heat capacity
+                    const double S,     // no idea
+                    const double E,     // ...?
+                    const double density
+                                    );
 
            
-} ;
+};
 
 
 
